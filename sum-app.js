34fa53ce818a0c72ab69414350905b3ab11a625d
Manuel Hohenadl <manoo089@googlemail.com>
2021-12-15 15:49:54 +0100

    fixed: sumApp solved

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,33 @@
 class SumApp {
-  // Add you solution here
+    constructor() {
+        this.numbers = [];
+    }
+
+    addNumber(n) {
+        this.numbers.push(n);
+    }
+
+    getSum() {
+        let sum = 0;
+        for (let i = 0; i < this.numbers.length; i++) {
+            sum += this.numbers[i];
+        }
+        console.log(sum);
+        return sum;
+    }
+
+    reset() {
+        this.numbers.splice(0, this.numbers.length);
+        // OR
+        // this.numbers = [];
+    }
 }
+
+/*
+const newApp = new SumApp();
+newApp.addNumber(5);
+newApp.addNumber(5);
+newApp.getSum();
+newApp.reset();
+newApp.getSum(); 
+*/
